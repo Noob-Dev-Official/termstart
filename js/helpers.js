d08309312c68dcafcebd8b799c407247c784491f
Muntasir <muntasirenamalhuda@gmail.com>
2021-06-21 22:06:53 +0100

    added noob theme, added date

--- js/helpers.js
+++ js/helpers.js
@@ -1,17 +1,53 @@
-function getTime() {
-  let date = new Date(),
-    min = date.getMinutes(),
-    hour = date.getHours();
+const monthNames = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
 
-  return (
-    "" + (hour < 10 ? "0" + hour : hour) + ":" + (min < 10 ? "0" + min : min)
-  );
+const dayNames = [
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thurday',
+	'Friday',
+	'Saturday',
+	'Sunday',
+];
+
+function getTime() {
+	let date = new Date(),
+		min = date.getMinutes(),
+		hour = date.getHours();
+
+	return (
+		'' + (hour < 10 ? '0' + hour : hour) + ':' + (min < 10 ? '0' + min : min)
+	);
+}
+
+function getDate() {
+	let date = new Date(),
+		day = dayNames[date.getDay() - 1],
+		dayNo = date.getDate(),
+		month = monthNames[date.getMonth()],
+		year = date.getFullYear();
+
+	return '' + day + ' ' + dayNo + ' ' + month + ' ' + year;
 }
 
 window.onload = () => {
-  document.getElementById("clock").innerHTML = getTime();
+	document.getElementById('clock').innerHTML = getTime();
+	document.getElementById('date').innerHTML = getDate();
 
-  setInterval(() => {
-    document.getElementById("clock").innerHTML = getTime();
-  }, 100);
+	setInterval(() => {
+		document.getElementById('clock').innerHTML = getTime();
+	}, 100);
 };
--- js/writers.js
+++ js/writers.js
@@ -3,110 +3,114 @@
 */
 
 function list_writer(output) {
-  if (Array.isArray(output)) {
-    const terminal = document.getElementById("links");
-    const outputNode = document.createElement("div");
-    outputNode.classList.add("ls");
-    let inner = "<table class='ls-links'>";
+	if (Array.isArray(output)) {
+		const terminal = document.getElementById('links');
+		const outputNode = document.createElement('div');
+		outputNode.classList.add('ls');
+		let inner = "<table class='ls-links'>";
 
-    half1 = output.slice(output.length / 2);
-    half2 = output.slice(0, output.length / 2);
+		half1 = output.slice(output.length / 2);
+		half2 = output.slice(0, output.length / 2);
 
-    if (output.length <= 6) {
-      inner =
-        inner +
-        output
-          .map(
-            (item) =>
-            `<th class="ls-item"><a target='_blank' href='${links[item.key]}'>${item.key}</a></th>`
-          )
-          .join("");
-    } else {
-      inner =
-        inner +
-        half1
-          .map(
-            (item) =>
-            `<th class="ls-item"><a target='_blank' href='${links[item.key]}'>${item.key}</a></th>`
-          )
-          .join("");
-      inner = inner + "<tr></tr>";
-      inner =
-        inner +
-        half2
-          .map(
-            (item) =>
-            `<th class="ls-item"><a target='_blank' href='${links[item.key]}'>${item.key}</a></th>`
-          )
-          .join(" ");
-    }
+		if (output.length <= 6) {
+			inner =
+				inner +
+				output
+					.map(
+						(item) =>
+							`<th class="ls-item"><a href='${links[item.key]}'>${
+								item.key
+							}</a></th>`,
+					)
+					.join('');
+		} else {
+			inner =
+				inner +
+				half1
+					.map(
+						(item) =>
+							`<th class="ls-item"><a href='${links[item.key]}'>${
+								item.key
+							}</a></th>`,
+					)
+					.join('');
+			inner = inner + '<tr></tr>';
+			inner =
+				inner +
+				half2
+					.map(
+						(item) =>
+							`<th class="ls-item"><a target='_blank' href='${
+								links[item.key]
+							}'>${item.key}</a></th>`,
+					)
+					.join(' ');
+		}
 
-    inner = inner + "</table>";
-    outputNode.innerHTML = inner;
+		inner = inner + '</table>';
+		outputNode.innerHTML = inner;
 
-    run_command("clear");
-    terminal.appendChild(outputNode);
-
-  }
+		run_command('clear');
+		terminal.appendChild(outputNode);
+	}
 }
-
 
 function theme_writer(item) {
-  const terminal = document.getElementById("links");
-  const outputNode = document.createElement("div");
-  outputNode.classList.add("ls");
-  let inner = "<list class='ls-links'>";
+	const terminal = document.getElementById('links');
+	const outputNode = document.createElement('div');
+	outputNode.classList.add('ls');
+	let inner = "<list class='ls-links'>";
 
-  inner =
-    inner +
-    item
-      .map(
-        (item) =>
-          `<li class="ls-item"> <a href="javascript:run_command('theme ${item.value}');">
-        ${item.value}</a></li>`
-      )
-      .join("");
+	inner =
+		inner +
+		item
+			.map(
+				(item) =>
+					`<li class="ls-item"> <a href="javascript:run_command('theme ${item.value}');">
+        ${item.value}</a></li>`,
+			)
+			.join('');
 
-  inner = inner + "</list>";
-  outputNode.innerHTML = inner;
-  run_command("clear");
-  terminal.appendChild(outputNode);
+	inner = inner + '</list>';
+	outputNode.innerHTML = inner;
+	run_command('clear');
+	terminal.appendChild(outputNode);
 }
 
-function writer(output = "") {
-  if (Array.isArray(output)) {
-    list_writer(output);
-  }
+function writer(output = '') {
+	if (Array.isArray(output)) {
+		list_writer(output);
+	}
 }
 
 function help_writer(command) {
-  const terminal = document.getElementById("links");
-  const outputNode = document.createElement("div");
-  outputNode.classList.add("ls");
-  let inner = "<div class='ls-links' align='center' style='margin: 20px;'>";
+	const terminal = document.getElementById('links');
+	const outputNode = document.createElement('div');
+	outputNode.classList.add('ls');
+	let inner = "<div class='ls-links' align='center' style='margin: 20px;'>";
 
-  inner += `<br> <p class="ls-item">${command.name} - ${command.description}</p> <br>`
+	inner += `<br> <p class="ls-item">${command.name} - ${command.description}</p> <br>`;
 
-  inner += `<p class="ls-item">Usage: ${command.usage}</p><br>`
+	inner += `<p class="ls-item">Usage: ${command.usage}</p><br>`;
 
-  inner += `<p class="ls-item" >${command.longdesc}</li><br>`
+	inner += `<p class="ls-item" >${command.longdesc}</li><br>`;
 
-  inner = inner + "</div>";
-  outputNode.innerHTML = inner;
-  document.getElementById("links").innerHTML = "";
-  terminal.appendChild(outputNode);
+	inner = inner + '</div>';
+	outputNode.innerHTML = inner;
+	document.getElementById('links').innerHTML = '';
+	terminal.appendChild(outputNode);
 }
 
 function command_writer(command) {
-  const terminal = document.getElementById("links");
-  const outputNode = document.createElement("div");
-  outputNode.classList.add("ls");
-  let inner = "<div class='ls-links' align='center' style='margin: 20px;'>";
+	const terminal = document.getElementById('links');
+	const outputNode = document.createElement('div');
+	outputNode.classList.add('ls');
+	let inner = "<div class='ls-links' align='center' style='margin: 20px;'>";
 
-  inner += `<p class="ls-item">${command} </p>`
+	inner += `<p class="ls-item">${command} </p>`;
 
-  inner = inner + "</div>";
-  outputNode.innerHTML = inner;
-  document.getElementById("links").innerHTML = "";
-  terminal.appendChild(outputNode);
+	inner = inner + '</div>';
+	outputNode.innerHTML = inner;
+	document.getElementById('links').innerHTML = '';
+	terminal.appendChild(outputNode);
 }
